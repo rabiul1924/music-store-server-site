13bc3befd73b41cf72a48cd00d30ef9b3ab28cc7
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-04-18 15:07:26 +0600

    i

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const ObjectId = require("mongodb").ObjectId;
 
 require("dotenv").config();
 const port = process.env.PORT || 5000
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000
 
 
 const app = express()
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -40,6 +42,15 @@ client.connect(err => {
   });
   })
 
+  app.get("/book/:_id", (req, res) => {
+    console.log(req.params._id);
+    productCollection.find({ _id: ObjectId(req.params._id) })
+
+        .toArray((err, documents) => {
+            res.send(documents[0]);
+        });
+});
+
 
   app.get("/reviews", (req, res) => {
     productCollectionForReviews.find().toArray((err, review) => {
@@ -56,6 +67,8 @@ app.post('/comment', (req, res) => {
     res.send(result.insertedCount > 0);
 });
 })
+
+
 
 
  
